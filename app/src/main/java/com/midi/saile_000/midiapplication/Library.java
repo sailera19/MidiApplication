35f1bf8ffe38ee3914db05e19b7b1e987069a321
l0wsk1ll3d <sailer.alex@googlemail.com>
2014-10-09 23:22:36 +0200

    optimize imports
    optimize imports

--- app/src/main/java/com/midi/saile_000/midiapplication/Library.java
+++ app/src/main/java/com/midi/saile_000/midiapplication/Library.java
@@ -1,29 +1,17 @@
 package com.midi.saile_000.midiapplication;
 
 import android.app.Activity;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
