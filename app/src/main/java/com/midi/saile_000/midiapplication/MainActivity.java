237e4ebdf52df603c7c24c062f636d2a0f995ac7
Doro <dorojackyjue@hotmail.de>
2014-10-10 10:07:46 +0200

    Rounded Buttons

--- app/src/main/java/com/midi/saile_000/midiapplication/MainActivity.java
+++ app/src/main/java/com/midi/saile_000/midiapplication/MainActivity.java
@@ -9,9 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.ToggleButton;
 
-import jp.kshoji.javax.sound.midi.InvalidMidiDataException;
 import jp.kshoji.javax.sound.midi.MidiSystem;
 import jp.kshoji.javax.sound.midi.MidiUnavailableException;
 
